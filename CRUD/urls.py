7e1182e418432960bb0a0827755207d016588ad6
ghozi <ghozimbuh@gmail.com>
2023-05-29 22:26:01 +0700

    initial commit

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     #Home
     path('', views.home, name="home"),
     #Add Product
-    path('add_product', views.add_product, name="add_product"),
+    path('add_product', views.add_product, name="add_product"), # type: ignore
     #View Product Individually
-    path('product/<str:product_id>', views.product, name="product"),
+    path('product/<str:product_id>', views.product, name="product"), # type: ignore
     #Edit Product
-    path('edit_product', views.edit_product, name="edit_product"),
+    path('edit_product', views.edit_product, name="edit_product"), # type: ignore
     #Delete Product
     path('delete_product/<str:product_id>', views.delete_product, name="delete_product"),
 ]
